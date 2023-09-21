683351998566d9ec0291a774b52c48b43377ee6e
emiridbest <udogwuemiri@gmail.com>
2023-09-21 07:53:26 +0100

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,325 @@
-import Image from 'next/image'
+"use client";
+import React, { useState, useEffect } from "react";
+import SuperfluidWidget from "@superfluid-finance/widget";
+import superTokenList from "@superfluid-finance/tokenlist";
+import { WagmiConfig } from "wagmi";
+import { ConnectButton, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import Web3 from "web3";
+import { contractAddress, contractAbi } from "../abi/malaFlix";
+import { wagmiConfig, chains } from "../components/wagmi";
+import { client, getOwnedBy } from "../components/utils";
+import productDetails from "../components/productDetails";
+import paymentDetails from "../components/paymentDetails";
+import AddVideo from "../components/AddVideo";
+import VideoCard from "../components/VideoCard";
+import { Footer, Sidebar, Header, VideoModal } from "../components/Index";
+
 
 export default function Home() {
+  const [walletAddress, setWalletAddress] = useState("");
+  const [existingLensWallet, setExistingLensWallet] = useState("");
+  const [nonExistingLensWallet, setNonExistingLensWallet] = useState("");
+  const [lensHandleExists, setLensHandleExists] = useState("");
+  const [video, setVideos] = useState([]);
+  const [account, setAccount] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [play, setPlay] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalVideoItem, setModalVideoItem] = useState(null); // Define modalVideoItem
+
+
+  const [found, setFound] = useState(false);
+
+  const get = async () => {
+    const modalElement = document.querySelector('connectModalOpen'); // Replace with the actual selector for your modal
+    const spanElement = modalElement.querySelector('span.MuiTypography-root');
+    const textContent = spanElement.textContent;
+
+    if (textContent.includes('Success! Your purchase was confirmed.')) {
+      // Perform actions when the success message is found
+      console.log('Success message found');
+      setFound(true); // Set a state variable to indicate that the message is found
+      // You can also close the modal or update the app's state here
+    } else {
+      // If the message is not found, wait for a while and check again
+      setTimeout(get, 100); // Adjust the delay (in milliseconds) as needed
+    }
+  };
+
+  useEffect(() => {
+    if (!found) {
+      get(); // Start the initial check
+    }
+  }, [found]);
+
+  const initializeWeb3 = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.enable();
+        const web3 = new Web3(window.ethereum);
+
+        const accounts = await web3.eth.getAccounts();
+        const contract = new web3.eth.Contract(contractAbi, contractAddress);
+
+        setAccount(accounts[0]);
+        setContract(contract);
+      } catch (error) {
+        console.error("Error initializing web3:", error);
+      }
+    } else {
+      console.log("MetaMask extension not found.");
+    }
+  };
+
+  useEffect(() => {
+    initializeWeb3();
+  }, []);
+
+  async function fetchProfile() {
+    try {
+      const response = await client.query({
+        query: getOwnedBy,
+        variables: {
+          address: walletAddress,
+        },
+      });
+
+      if (response.data.defaultProfile == null) {
+        setLensHandleExists("false");
+        setNonExistingLensWallet(walletAddress);
+        setExistingLensWallet("");
+      } else {
+        setLensHandleExists("true");
+        setExistingLensWallet(walletAddress);
+        setNonExistingLensWallet("");
+      }
+    } catch (e) {
+      console.log("Error:", e);
+    }
+  }
+
+  const handleChange = (e) => {
+    setWalletAddress(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    document.querySelector("#inputField").value = "";
+    fetchProfile();
+  };
+
+  const customPaymentDetails = paymentDetails.paymentOptions.map((option) => {
+    return {
+      ...option,
+      receiverAddress: existingLensWallet,
+    };
+  });
+
+  const fetchVideoList = async () => {
+    try {
+      const videoIds = await contract.methods.getAllVideos().call();
+
+      const formattedVideos = [];
+
+      for (const videoIdBN of videoIds) {
+        const videoIdString = videoIdBN.toString();
+        const videoId = parseInt(videoIdString);
+        console.log("Real video IDs:", videoId);
+
+        const videoDetail = await contract.methods.videoList(videoId).call();
+        formattedVideos.push({ ...videoDetail, key: videoId });
+        console.log("Raw video IDs:", videoDetail);
+        setVideos(formattedVideos);
+      }
+    } catch (error) {
+      console.error("Error fetching video list:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchVideoList();
+  }, [contract, account]);
+
+
+  const handleWatchVideo = async (videoItem) => {
+
+    try {
+      if (videoItem.creator) {
+        console.log(videoItem.creator);
+        setWalletAddress(videoItem.creator);
+      }
+
+      if(!play) {
+        setLensHandleExists("true");
+        setExistingLensWallet(walletAddress);
+        setNonExistingLensWallet("");
+      }
+     if(play && videoItem.videoURL) {
+        console.log(videoItem.videoURL);
+      setModalVideoItem(videoItem.videoURL);
+      setIsModalOpen(true);
+
+      }
+
+      // Call your existing functions to check Lens Protocol
+    } catch (error) {
+      console.error("Error watching video:", error);
+    }
+  };
+  useEffect(() => {
+    if(play) {
+    handleWatchVideo();
+  }
+  }, [play]);
+  async function addVideo(title, description, thumbnail, CID) {
+    try {
+      console.log(title, description, thumbnail, CID);
+
+      if (!contract || !account) {
+        console.error(
+          "Contract or account not available. Make sure you are connected to MetaMask."
+        );
+        return;
+      }
+
+      const tx = await contract.methods.addVideo(title, description, thumbnail, CID);
+
+      // Send the transaction
+      const txResponse = await window.ethereum.request({
+        method: "eth_sendTransaction",
+        params: [
+          {
+            from: account,
+            to: contractAddress,
+            data: tx.encodeABI(),
+          },
+        ],
+      });
+      // Wait for the transaction to be mined
+      const receipt = await txResponse.wait();
+
+      if (receipt.status === 1) {
+        console.log("Video uploaded successfully!");
+        fetchVideoList(); // Refresh the video list after upload
+      } else {
+        console.error("Transaction failed.");
+      }
+    } catch (error) {
+      console.error("Error uploading video:", error);
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="container mx-auto p-1">
+      <Header />
+      <div>
+      <Sidebar />
+      {isModalOpen && modalVideoItem && (
+        <VideoModal
+          videoURL={modalVideoItem}
+          onClose={() => setIsModalOpen(false)}
         />
+      )}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 m-10 ">
+        {video.map((videoItem) => (
+       <VideoCard
+       key={videoItem.id}
+       videoItem={{
+         title: videoItem.title,
+         description: videoItem.description,
+         creator: videoItem.creator,
+         thumbnail: `https://gateway.pinata.cloud/ipfs/${videoItem.thumbnail}`,
+         videoURL: `https://gateway.pinata.cloud/ipfs/${videoItem.CID}`,
+         id: videoItem.id,
+       }}
+       handleWatchVideo={handleWatchVideo}
+     />
+   ))}
+ </div>
+      <div/>
       </div>
+      <section className="hidden">
+        <section className="w-1/2 ml-10">
+          <input
+            className="w-2/3 h-10 mr-4 pl-2 text-black outline-none border-[#54b840] border-2 rounded-lg"
+            type="text"
+            id="inputField"
+            name="inputField"
+            maxLength="120"
+            required
+            onChange={handleChange}
+          />
+          <button
+            className="bg-[#54b840] hover:bg-[#52aa41] w-1/8 h-10 px-10 outline-none border-none rounded-lg cursor-pointer"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </section>
+        <section className="w-1/2 text-black pr-10">
+          <section className="text-black">
+            {lensHandleExists === "true" && (
+              <section className="text-black">
+                <h2>
+                  <span className="font-medium">{existingLensWallet}</span> has
+                  a Lens handle. You can start a stream
+                </h2>
+              </section>
+            )}
+            {lensHandleExists === "false" && (
+              <section>
+                {nonExistingLensWallet} doesn't have a Lens handle
+              </section>
+            )}
+          </section>
+        </section>
+      </section>
+      <section className="h-20 flex justify-center items-center mt-6 mx-10">
+        {lensHandleExists === "true" && (
+          <WagmiConfig config={wagmiConfig}>
+            <RainbowKitProvider chains={chains}>
+              <ConnectButton.Custom>
+                {({ openConnectModal, connectModalOpen }) => {
+                  const walletManager = {
+                    open: async () => openConnectModal(),
+                    isOpen: connectModalOpen,
+                  };
+                  return (
+                    <section>
+                      <SuperfluidWidget
+                                   productDetails={productDetails}
+                        paymentDetails={{
+                          paymentOptions: customPaymentDetails,
+                        }}
+                        tokenList={superTokenList}
+                        type="dialog"
+                        walletManager={walletManager}
+                        onSuccess={() => {
+                          setPlay(true);
+                          console.log("onSuccess");
+                          closeModal();
+                        }} >
+                        {({ openModal }) => (
+                          <button
+                            onClick={() => {
+                              openModal();
+                            }}
+                            className="bg-red-600 hover:bg-red-500 h-16 px-12 font-medium outline-none border-none rounded-full cursor-pointer"
+                          >
+                            Start Stream
+                          </button>
+                        )}
+                      </SuperfluidWidget>
+                    </section>
+                  );
+                }}
+              </ConnectButton.Custom>
+            </RainbowKitProvider>
+          </WagmiConfig>
+        )}
+      </section>
+      <AddVideo addVideo={addVideo} />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Footer />
     </main>
-  )
+  );
 }
---
+++ src/components/productDetails.js
@@ -0,0 +1,7 @@
+const productDetails = {
+    name: "MalaFlix",
+    description:
+      "MalaFlix is a subscription-based video streaming service for continuous & perpetual fun.",
+  };
+  
+  export default productDetails;
