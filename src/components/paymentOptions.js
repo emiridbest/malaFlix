bb34484f036fb8ce317913c5399d10a51919f66f
emiridbest <Udogwuemiri@gmail.com>
2024-03-05 22:33:44 +0100

    Update paymentOptions.js

--- src/components/paymentOptions.js
+++ src/components/paymentOptions.js
@@ -1,34 +1,12 @@
 const paymentOptions = [
   {
-    chainId: 80001,
+    chainId: 84531,
     receiverAddress: "0x0000000000000000000000000000000000000000",
     superToken: {
-      address: "0x5d8b4c2554aeb7e86f387b4d6c00ac33499ed01f",
+      address: "0x529a4116f160c833c61311569d6b33dff41fd657",
     },
     flowRate: {
-      amountEther: "1",
-      period: "month",
-    },
-  },
-  {
-    chainId: 80001,
-    receiverAddress: "0x0000000000000000000000000000000000000000",
-    superToken: {
-      address: "0x42bb40bf79730451b11f6de1cba222f17b87afd7",
-    },
-    flowRate: {
-      amountEther: "1",
-      period: "month",
-    },
-  },
-  {
-    chainId: 100,
-    receiverAddress: "0x0000000000000000000000000000000000000000",
-    superToken: {
-      address: "0x59988e47a3503aafaa0368b9def095c818fdca01",
-    },
-    flowRate: {
-      amountEther: "1",
+      amountEther: "0.001",
       period: "month",
     },
   },
