bce1f624fb9c35234bcec4e5f491c83884a8b038
emiridbest <Udogwuemiri@gmail.com>
2024-03-05 23:42:37 +0100

    Update paymentOptions.js

--- src/components/paymentOptions.js
+++ src/components/paymentOptions.js
@@ -1,6 +1,6 @@
 const paymentOptions = [
   {
-    chainId: 84531,
+    chainId: 56,
     receiverAddress: "0x0000000000000000000000000000000000000000",
     superToken: {
       address: "0x529a4116f160c833c61311569d6b33dff41fd657",
